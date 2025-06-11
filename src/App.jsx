38bc8471c3fa673fa1291090faf8109bee523583
Daniel Biel <daniel.biel@glocalfoundation.ca>
2025-06-11 15:13:19 -0400

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import Navbar from './components/Navbar'
 import Dashboard from './components/Dashboard'
 
 export default function App() {
+  console.log("opened successfully")
   return (
     <Router>
       <div className="flex flex-col w-screen h-screen bg-gray-900 text-white overflow-hidden">
         {/* Content area */}
         <div className="flex-1 overflow-auto">
           <div className="mx-auto w-full max-w-screen-2xl h-full p-6">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-            </Routes>
+            <Dashboard />
           </div>
         </div>
       </div>
