30a646adc95cb2fb26bcccad9c3d85813a954283
Daniel Biel <daniel.biel@glocalfoundation.ca>
2025-06-11 14:58:24 -0400

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/municipality-visualizer/',
   plugins: [
     react(),
     tailwindcss()
   ],
+  build: {
+    outDir: 'dist'
+  }
 })
